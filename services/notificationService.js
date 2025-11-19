eda4fb08f6ea106f3437929413bc13916a170f93
anhpmt <anhpmt@hd-global-consulting.com>
2025-11-19 17:58:51 +0700

    Fix store token fcm

--- services/notificationService.js
+++ services/notificationService.js
@@ -1,7 +1,7 @@
 import messaging from '@react-native-firebase/messaging';
+import * as Device from 'expo-device';
 import { Alert } from 'react-native';
 import axiosClient from '../api/axiosClient';
-
 
 async function requestUserPermission() {
   const authStatus = await messaging().requestPermission();
@@ -17,17 +17,17 @@ async function requestUserPermission() {
 
 async function getFCMToken() {
     try {
-        const response = await axiosClient.get('/getFcmToken.php');
-        console.log('reponse getFcmToken', response);
-        if (response && response.success) {
-          console.log('get token fcm from db');
-          return response.data.token_fcm;
-        }
+        
         const token = await messaging().getToken();
         console.log("FCM Token:", token);
         // Save the token to your server
         const formData = new FormData();
         formData.append('token', token);
+        formData.append('device_model', Device.modelName);
+        formData.append('device_brand', Device.brand);
+        formData.append('platform', Device.osName);
+        formData.append('os_version', Device.osVersion);
+
         console.log('Sending token to server:', formData);
         await axiosClient.post('/saveToken.php', formData, {
             headers: {
@@ -42,18 +42,6 @@ async function getFCMToken() {
             status: error.status,
             data: error.data
         });
-        const token = await messaging().getToken();
-        console.log("FCM Token:", token);
-        // Save the token to your server
-        const formData = new FormData();
-        formData.append('token', token);
-        console.log('Sending token to server:', formData);
-        await axiosClient.post('/saveToken.php', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-        });
-        console.log('Token saved to server successfully.');
     }
     return null;
 }
